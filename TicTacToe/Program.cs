52c3f2f2cd280597490d9a77f36f52a1cfba5b11
Diyar Faraj <diyar.faraj@gmail.com>
2020-04-28 18:18:01 +0200

    hejhej

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+
+        static char[,] playField = {
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'}
+            };
+        
+
+        static int turns = 0; 
+        static void Main(string[] args)
+        {
+            int player = 2;
+            int input = 0;
+            bool inputCorrect = true; 
+
+
+            SetField();
+            
+
+
+            do // run game as long as it's true
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXorO(player, input);
+                } else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player, input);
+                }
+
+                SetField();
+                #region
+                //Check for the winner
+                char[] playerChars = { 'X', 'O' };
+
+                foreach(char playerChar in playerChars)
+                {
+                    if(((playField[0,0] == playerChar) && (playField[0, 1] == playerChar) && (playField[0, 2] == playerChar)) 
+                        || ((playField[1, 0] == playerChar) && (playField[1, 1] == playerChar) && (playField[1, 2] == playerChar))
+                        || ((playField[2, 0] == playerChar) && (playField[2, 1] == playerChar) && (playField[2, 2] == playerChar))
+                        || ((playField[0, 0] == playerChar) && (playField[1, 0] == playerChar) && (playField[2, 0] == playerChar))
+                        || ((playField[0, 1] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 1] == playerChar))
+                        || ((playField[0, 2] == playerChar) && (playField[1, 2] == playerChar) && (playField[2, 2] == playerChar))
+                        || ((playField[0, 2] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 0] == playerChar))
+                        || ((playField[0, 0] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 2] == playerChar))
+                        )
+                    {
+                        if(playerChar == 'X')
+                        {
+                            Console.WriteLine("\n Player 2 is a winner, chicken dinner");
+                        } else
+                        {
+                            Console.WriteLine("\n Player 1 is a winner, chicken dinner");
+                        }
+
+                        Console.WriteLine("RESET THE GAME? please enter any key");
+                        Console.ReadKey();
+
+                        // Reset field
+                        resetPlay();
+                        break;
+                    } else if (turns == 10)
+                    {
+                        //if we dont have winner 
+                        Console.WriteLine("\n Game is DRAW. NO WINNERS or DINNER. Press any key to restart");
+                        Console.ReadKey();
+                        resetPlay();
+                        break;
+
+                    }
+                }
+
+
+                #endregion
+
+                #region
+                // asking for input, seting the field, and
+                do
+                {
+                    Console.Write("\n Player {0} choose your field: ", player);
+                    try
+                    {
+                        input = Convert.ToInt32(Console.ReadLine());
+
+                    }
+                    catch 
+                    {
+
+                        Console.WriteLine("WRONG INPUT MOTHERFUCKER");
+                    }
+
+                    if ((input == 1) && (playField[0, 0] == '1'))
+                        inputCorrect = true;
+                    else if ((input == 2) && (playField[0, 1] == '2'))
+                        inputCorrect = true;
+                    else if ((input == 3) && (playField[0, 2] == '3'))
+                        inputCorrect = true;
+                    else if ((input == 4) && (playField[1, 0] == '4'))
+                        inputCorrect = true;
+                    else if ((input == 5) && (playField[1, 1] == '5'))
+                        inputCorrect = true;
+                    else if ((input == 6) && (playField[1, 2] == '6'))
+                        inputCorrect = true;
+                    else if ((input == 7) && (playField[2, 0] == '7'))
+                        inputCorrect = true;
+                    else if ((input == 8) && (playField[2, 1] == '8'))
+                        inputCorrect = true;
+                    else if ((input == 9) && (playField[2, 2] == '9'))
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("\n Incorrect input. Please use ANOTHER ONE");
+                        inputCorrect = false;
+                    }
+
+                } while (!inputCorrect);
+                #endregion
+
+            } while (true);
+          
+        }
+
+        //resetting method 
+        public static void resetPlay()
+        {
+             char[,] playFieldInitial = {
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'}
+            };
+
+            playField = playFieldInitial;
+            SetField();
+            turns = 0;
+
+        }
+
+        // METHOD to print fields
+        public static void SetField()
+        {
+            Console.Clear();
+            Console.WriteLine(" {0} {1} {2}", "---", "---", "---");
+            Console.WriteLine("| {0} | {1} | {2} |", playField[0, 0], playField[0, 1], playField[0, 2]);
+            Console.WriteLine("|{0}|{1}|{2}|", "---", "---", "---");
+            Console.WriteLine("| {0} | {1} | {2} |", playField[1, 0], playField[1, 1], playField[1, 2]);
+            Console.WriteLine("|{0}|{1}|{2}|", "---", "---", "---");
+            Console.WriteLine("| {0} | {1} | {2} |", playField[2, 0], playField[2, 1], playField[2, 2]);
+            Console.WriteLine(" {0} {1} {2}", "---", "---", "---");
+            turns++; 
+        }
+
+
+        // METHOD to validate userinput
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+
+
+            if (player == 1)
+                playerSign = 'X';
+            else if (player == 2)
+                playerSign = 'O';
+
+
+
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
+
+            
+        }
+        
+    }
+}
